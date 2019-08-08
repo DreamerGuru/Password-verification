00dfbf8f792e05ed5da12f080e71fd54336fcb1b
Alekh Bajpai <47384858+DreamerGuru@users.noreply.github.com>
2019-08-08 15:41:34 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,7 @@
+password = ""
+while password != 'Alekh123':
+    password = input("Entre the password")
+    if password == 'Alekh123':
+        print("You're logged in")
+    else:
+        print("Sory, try again")
